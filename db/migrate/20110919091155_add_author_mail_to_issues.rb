68efe6659f079bc339f0cc1c422421884d89ab02
Niremizov <niremizov@yandex.ru>
2017-05-04 23:36:35 +0300

    Updated migration to compatibility with ruby 2.2

--- db/migrate/20110919091155_add_author_mail_to_issues.rb
+++ db/migrate/20110919091155_add_author_mail_to_issues.rb
@@ -1,11 +1,11 @@
 class AddAuthorMailToIssues < ActiveRecord::Migration
   def self.up
     unless Issue.column_names.include? "author_name"
-      add_column :issues, :author_name, :string, :null => true
+      add_column :issues, :author_name, :string, null: true
       add_index :issues, [:author_name]
     end
     unless Issue.column_names.include? "author_mail"
-      add_column :issues, :author_mail, :string, :null => true
+      add_column :issues, :author_mail, :string, null: true
       add_index :issues, [:author_mail]
     end
   end
--- db/migrate/20110920033709_add_user_mail_and_name_to_journals.rb
+++ db/migrate/20110920033709_add_user_mail_and_name_to_journals.rb
@@ -1,11 +1,11 @@
 class AddUserMailAndNameToJournals < ActiveRecord::Migration
   def self.up
     unless Journal.column_names.include? "user_mail"
-      add_column :journals, :user_mail, :string, :null => true
+      add_column :journals, :user_mail, :string, null: true
       add_index :journals, [:user_mail]
     end
     unless Journal.column_names.include? "user_name"
-      add_column :journals, :user_name, :string, :null => true
+      add_column :journals, :user_name, :string, null: true
       add_index :journals, [:user_name]
     end
   end
